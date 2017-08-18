b5a0be226e3ce005d04e40a36a3f58963abb511b
Eugene Dubovik <edubovik90@gmail.com>
2017-08-18 12:33:16 +0300

    Added link check;

--- app/src/main/java/com/demo/bankexdh/model/store/RealmContract.java
+++ app/src/main/java/com/demo/bankexdh/model/store/RealmContract.java
@@ -7,7 +7,7 @@ import io.realm.RealmConfiguration;
 
 public class RealmContract {
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "bankex.realm";
 
     public static void configureRealm(Context context) {
