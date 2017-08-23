6c8982609784102c148beaa206a1bce492cee9f7
Eugene Dubovik <edubovik90@gmail.com>
2017-08-23 12:44:43 +0300

    Updated version; Erased hash suffix;

--- app/src/main/java/com/demo/bankexdh/utils/ImageUtils.java
+++ app/src/main/java/com/demo/bankexdh/utils/ImageUtils.java
@@ -102,9 +102,9 @@ public class ImageUtils {
         byte[] mdbytes = md.digest();
 
         //convert the byte to hex format method 1
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < mdbytes.length; i++) {
-            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        StringBuilder sb = new StringBuilder();
+        for (byte mdbyte : mdbytes) {
+            sb.append(Integer.toString((mdbyte & 0xff) + 0x100, 16).substring(1));
         }
 
         return sb.toString();
