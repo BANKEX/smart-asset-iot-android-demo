2db8ad48cbb316e0679a96bcf473bafca5c2bfa2
Eugene Dubovik <edubovik90@gmail.com>
2017-08-23 17:48:30 +0300

    Updated ID; Logo; Deleted ru localization;

--- app/src/main/java/com/demo/bankexdh/model/store/RealmContract.java
+++ app/src/main/java/com/demo/bankexdh/model/store/RealmContract.java
@@ -7,7 +7,7 @@ import io.realm.RealmConfiguration;
 
 public class RealmContract {
 
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 5;
     public static final String DATABASE_NAME = "bankex.realm";
 
     public static void configureRealm(Context context) {
