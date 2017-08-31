2dbcf35331a491cfe1b6ba24b252fc2d3544b139
Eugene Dubovik <edubovik90@gmail.com>
2017-08-31 12:36:33 +0300

    Updated registration UI and logic;

--- app/src/main/java/com/demo/bankexdh/view/activity/RegistrationActivity.java
+++ app/src/main/java/com/demo/bankexdh/view/activity/RegistrationActivity.java
@@ -1,18 +1,16 @@
-package com.demo.bankexdh.view.fragment;
+package com.demo.bankexdh.view.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.view.LayoutInflater;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.demo.bankexdh.R;
-import com.demo.bankexdh.presenter.base.BasePresenterFragment;
+import com.demo.bankexdh.presenter.base.BasePresenterActivity;
 import com.demo.bankexdh.presenter.base.PresenterFactory;
 import com.demo.bankexdh.presenter.base.RegistrationView;
 import com.demo.bankexdh.presenter.impl.RegistrationPresenter;
@@ -21,18 +19,19 @@ import com.google.zxing.integration.android.IntentResult;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.RuntimePermissions;
 
-public class RegistrationFragment
-        extends BasePresenterFragment<RegistrationPresenter, RegistrationView>
+@RuntimePermissions
+public class RegistrationActivity extends BasePresenterActivity<RegistrationPresenter, RegistrationView>
         implements RegistrationView {
 
     @BindView(R.id.parent_container)
     View parentContainer;
     @BindView(R.id.asset_id_edit)
     EditText assetIdEdit;
-    @BindView(R.id.scan_button)
-    View scanButton;
-
     private RegistrationPresenter presenter;
 
     private boolean scannedContentReceived;
@@ -49,24 +48,41 @@ public class RegistrationFragment
         this.presenter = presenter;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_registration, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.NoActionBar);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.scan_button)
+    void initiateScan() {
+        RegistrationActivityPermissionsDispatcher.startScanWithCheck(this);
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    public void startScan() {
+        new IntentIntegrator(this)
+                .setPrompt(getString(R.string.scan_barcode_prompt))
+                .initiateScan();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void showNeverAskAgain() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.permission_dialog_title)
+                .setMessage(R.string.permission_dialog_message)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                    dialog.dismiss();
+                }).setNegativeButton(getString(android.R.string.cancel),
+                (dialog, which) -> dialog.dismiss()).show();
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        scanButton.setOnClickListener(button -> {
-            IntentIntegrator.forSupportFragment(this)
-                    .setPrompt(getString(R.string.scan_barcode_prompt))
-                    .initiateScan();
-        });
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        RegistrationActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     @Override
@@ -102,11 +118,17 @@ public class RegistrationFragment
     @Override
     public void showAssetId(String assetId) {
         assetIdEdit.setText(assetId);
+        assetIdEdit.setSelection(assetId.length());
     }
 
     @Override
     public void showGetAssetIdError() {
         Snackbar.make(parentContainer, R.string.error_get_asset_id, Snackbar.LENGTH_SHORT)
                 .show();
+    }
+
+    @Override
+    protected void setupActivityComponent() {
+
     }
 }
