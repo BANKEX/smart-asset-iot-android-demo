65de2248110ad741ef4df4ff78ab3f92fe5904c2
Eugene Dubovik <edubovik90@gmail.com>
2017-09-01 14:26:19 +0300

    Updated Urls;

--- app/src/main/java/com/demo/bankexdh/utils/Const.java
+++ app/src/main/java/com/demo/bankexdh/utils/Const.java
@@ -2,8 +2,8 @@ package com.demo.bankexdh.utils;
 
 public class Const {
 
-    public static final String URL = "http://13.94.247.153/api/rest/";
-    public static final String REGISTER_URL = "http://13.94.247.153:5000/register";
+    public static final String URL = "https://devicehive.bankex.org/api/rest/";
+    public static final String REGISTER_URL = "https://devicehive.bankex.org/register";
     public static final String DEVICE_ID_FORMAT = "%1$s-%2$s-%3$s";
     public static final String NOTIFICATION_NAME_FORMAT = "%1$s-%2$s";
     public static final String KEY = "4TzFLFXkMEILDHsbQHVIX1u93TXGpCg5";
